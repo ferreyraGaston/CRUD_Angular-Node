9390597cfae5becb893dc457b1dbd1b7eeb611a9
ferreyraGaston <ferreyragaston351@gmail.com>
2023-06-05 08:55:08 -0300

    crearProducto

--- Setvidor/controllers/productoController.js
+++ Setvidor/controllers/productoController.js
@@ -1,3 +1,16 @@
-exports.crearProducto = (req,res)=>{
-    console.log(req.body);
+const Producto = require("../models/Producto");
+
+exports.crearProducto =async (req,res)=>{
+    try{
+        let producto;
+        //creamos nuestros productos
+        producto=new Producto(req.body);
+        await producto.save();
+
+        res.send(producto);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('hubo un error al Crear el Producto')
+    }
 }
