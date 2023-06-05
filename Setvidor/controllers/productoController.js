25683a5596b088e574763300ed98e2785e27aa0f
ferreyraGaston <ferreyragaston351@gmail.com>
2023-06-05 09:43:18 -0300

    eliminar producto

--- Setvidor/controllers/productoController.js
+++ Setvidor/controllers/productoController.js
@@ -14,3 +14,74 @@ exports.crearProducto =async (req,res)=>{
         res.status(500).send('hubo un error al Crear el Producto')
     }
 }
+
+
+exports.obtenerProdctos =async (req,res)=>{
+    try{
+        const productos= await Producto.find();
+
+
+        res.json(productos);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('hubo un error al Obtener el Producto')
+    }
+}
+
+exports.actualizarProdctos=async (req,res)=>{
+    try{
+        const {nombre,categoria,ubicacion,precio}=req.body;
+
+        let producto = await Producto.findById(req.params.id);
+        if(!producto){
+            res.status(404).json({msg: 'El producto no existe'});
+        }
+
+        producto.nombre=nombre;
+        producto.categoria=categoria;
+        producto.ubicacion=ubicacion
+        producto.precio=precio;
+
+        // para hacer la actualizacion
+        producto=await Producto.findByIdAndUpdate({_id:req.params.id},producto,{new:true})
+
+        res.json(producto);
+     
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('hubo un error al Obtener el Producto')
+    }
+}
+
+exports.obtenerUnProdcto=async (req,res)=>{
+    try{
+        let producto = await Producto.findById(req.params.id);
+        if(!producto){
+            res.status(404).json({msg: 'El producto no existe'});
+        }
+
+        res.json(producto);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('hubo un error al Obtener el Producto')
+    }
+}
+
+exports.eliminarProdcto= async (req,res)=>{
+    try{
+        let producto = await Producto.findById(req.params.id);
+        if(!producto){
+            res.status(404).json({msg: 'El producto no existe'});
+        }
+        await Producto.findByIdAndRemove({_id:req.params.id});
+
+        res.json({msg: 'Producto eliminado con exito'});
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).send('hubo un error al eliminar el Producto')
+    }
+}
--- Setvidor/routes/producto.js
+++ Setvidor/routes/producto.js
@@ -4,6 +4,16 @@ const router = express.Router();
 const productoController= require('../controllers/productoController');
 
 //api/productos
+
+// enviamos a la base de datos los productos.
 router.post('/',productoController.crearProducto);
+// extraemos de la base de datos los productos.
+router.get('/',productoController.obtenerProdctos);
+// actualizacion en la base de datos los productos.
+router.put('/:id',productoController.actualizarProdctos);
+// Obtener un producto en la base de datos
+router.get('/:id',productoController.obtenerUnProdcto);
+// Eliminar un producto en la base de datos
+router.delete('/:id',productoController.eliminarProdcto);
 
 module.exports = router;
